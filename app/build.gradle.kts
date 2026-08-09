c32d7fe5b6a319b00b0991812dbafdb93f47df5e
wrimoldi-byte <wrimoldi@gmail.com>
2026-08-09 15:29:17 -0300

    Bump WLive to v0.6.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.walter.wlive"
         minSdk = 26
         targetSdk = 35
-        versionCode = 5
-        versionName = "0.5.0"
+        versionCode = 6
+        versionName = "0.6.0"
     }
 
     buildFeatures { viewBinding = false }
